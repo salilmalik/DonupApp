921c4de48838f57e24f469f54af30bddc5e6a679
Malik,Salil <salilmalik92@gmail.com>
2016-04-11 23:37:32 +0530

    Changed to tab layout

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,182 +1,28 @@
-angular
-    .module('starter.controllers', [])
+angular.module('starter.controllers', [])
 
-    .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('DashCtrl', function($scope) {})
 
-      // With the new view caching in Ionic, Controllers are only called
-      // when they are recreated or on app start, instead of every page
-      // change.
-      // To listen for when this page is active (for example, to refresh
-      // data),
-      // listen for the $ionicView.enter event:
-      // $scope.$on('$ionicView.enter', function(e) {
-      // });
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
 
-      // Form data for the login modal
-      $scope.loginData = {};
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
 
-      // Create the login modal that we will use later
-      $ionicModal.fromTemplateUrl('templates/login.html', {
-        scope : $scope
-      }).then(function(modal) {
-        $scope.modal = modal;
-      });
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
 
-      // Triggered in the login modal to close it
-      $scope.closeLogin = function() {
-        $scope.modal.hide();
-      };
-
-      // Open the login modal
-      $scope.login = function() {
-        $scope.modal.show();
-      };
-
-      // Perform the login action when the user submits the login form
-      $scope.doLogin = function() {
-        console.log('Doing login', $scope.loginData);
-
-      };
-
-    })
-
-    .controller('PlaylistsCtrl', function($scope) {
-      $scope.playlists = [ {
-        title : 'Reggae',
-        id : 1
-      }, {
-        title : 'Chill',
-        id : 2
-      }, {
-        title : 'Dubstep',
-        id : 3
-      }, {
-        title : 'Indie',
-        id : 4
-      }, {
-        title : 'Rap',
-        id : 5
-      }, {
-        title : 'Cowbell',
-        id : 6
-      } ];
-    })
-
-    .controller(
-        'LoginCtrl',
-        function($scope, $window, $state, $cordovaOauth, UserService,
-            $localstorage) {
-          $scope.user = {};
-          $scope.isUserRegistered = false;
-          $scope.message = 'sdasd';
-          $scope.loginUser = function() {
-            UserService.loginUser($scope.user).success(
-                function(data) {
-                  if (data.success == true) {
-                    window.localStorage.setItem(
-                        "usertoken", data.token);
-                    console.log('data.token' + data.token);
-                    console.log('TOKEN IN LOCAL STORAGE'
-                        + window.localStorage
-                            .getItem("usertoken"));
-                    window.localStorage.setItem("username",
-                        $scope.user.username);
-                    $scope.message = data.message;
-                    $state.go('tab.chats');
-                  }
-                  console.log(data.message);
-                  $scope.message = data.message;
-                  $scope.user.password = '';
-                });
-          }
-          $scope.googleLogin = function() {
-            $cordovaOauth
-                .google(
-                    "673944921770-cv6rdg7rgtcqu4p15ob12brc3v1u6963.apps.googleusercontent.com",
-                    [ "email" ])
-                .then(
-                    function(result) {
-                      $scope.access_token = result.access_token;
-                      ProfileService
-                          .getProfileGoogle(
-                              $scope.access_token)
-                          .success(
-                              function(data) {
-                                user.name = data.displayName;
-                                user.email = data.emails[0].value;
-                                user.imageURL = data.image.url
-                                saveUserData(user);
-                              });
-                    }, function(error) {
-                    });
-          }
-          function saveUserData(user) {
-            UserService.saveUserData(user).success(function(data) {
-              $scope.userID = data.objectId;
-              $localstorage.setObject('user', user);
-              $state.go($state.current, {}, {
-                reload : true
-              });
-            });
-          }
-          $scope.facebookLogin = function() {
-            $cordovaOauth
-                .facebook(
-                    "758110324290388",
-                    [ "email", "public_profile",
-                        "user_friends" ])
-                .then(
-                    function(result) {
-                      $scope.access_token = result.access_token;
-                      ProfileService
-                          .getProfileFacebook(
-                              $scope.access_token)
-                          .success(
-                              function(data) {
-                                user.name = data.name;
-                                user.email = data.email;
-                                user.imageURL = data.picture.data.url;
-                                saveUserData(user);
-                              });
-                    }, function(error) {
-                    });
-          }
-        })
-    .controller(
-        'RegisterCtrl',
-        function($scope, UserService) {
-           $scope.user = {};
-          $scope.registerUser = function() {
-            console.log($scope.user.username);
-            UserService
-                .registerUser($scope.user)
-                .success(
-                    function(data) {
-                      if (data.message == 'User created!') {
-                        console.log('SUCCESS');
-                        $scope.isUserRegistered = true;
-                        var alertPopup = $ionicPopup
-                            .alert({
-                              title : 'Successfully Registered.',
-                              template : 'Successfully Registered. Please login.',
-                            });
-                        $state.go('tab.login');
-                      }
-                      console.log('ERROR');
-                      $scope.message = data.message;
-                      var alertPopup = $ionicPopup
-                          .alert({
-                            title : 'Error.',
-                            template : $scope.message,
-                          });
-
-                      $scope.register = {};
-                      /*
-                       * if(data.success===false){
-                       * $window.location.reload(); }
-                       */
-
-                    });
-          }
-        }).controller('PlaylistCtrl', function($scope, $stateParams) {
-    });
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
--- www/js/services.js
+++ www/js/services.js
@@ -1,103 +1,50 @@
 angular.module('starter.services', [])
 
-.factory('UserService', [ '$http', function($http) {
-	return {
-		registerUser : registerUser,
-		loginUser : loginUser,
-		getUser : getUser
-	};
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
 
-	function registerUser(user) {
-		return $http({
-			method : 'POST',
-			url : 'http://54.69.21.130/api/user',
-			data : user
-		})
-	}
-	;
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png'
+  }];
 
-	function loginUser(user) {
-		return $http({
-			method : 'POST',
-			url : 'http://54.69.21.130/api/login',
-			data : user
-		})
-	}
-	;
-	function getUser(user) {
-		return $http({
-			method : 'GET',
-			url : 'http://54.69.21.130/api/user/' + user.userID,
-			headers : {
-				'x-access-token' : user.token
-			}
-		})
-	}
-	;
-
-} ]).factory('ImageService', [ '$http', function($http) {
-	return {
-		getImage : getImage,
-		getUserImages : getUserImages,
-		getmultiImage : getmultiImage,
-		updatePoints : updatePoints,
-		updateUserPoints : updateUserPoints
-	};
-	function getImage(imageId) {
-		return $http({
-			method : 'GET',
-			url : '/api/image/' + imageId
-		})
-	}
-	;
-	function getUserImages(userId) {
-		return $http({
-			method : 'GET',
-			url : '/api/image/getUserImages/' + userId
-		})
-	}
-	;
-	function getmultiImage(multiImage) {
-		return $http({
-			method : 'GET',
-			url : '/api/image/multiImage/' + multiImage
-
-		})
-	}
-	;
-	function updatePoints(imageId) {
-		return $http({
-			method : 'PUT',
-			url : '/api/image/' + imageId
-
-		})
-	}
-	;
-	function updateUserPoints(userID) {
-		return $http({
-			method : 'PUT',
-			url : '/api/user/' + userID
-		})
-	}
-	;
-
-} ]).factory('$localstorage', [ '$window', function($window) {
-	return {
-		set : function(key, value) {
-			$window.localStorage[key] = value;
-		},
-		get : function(key, defaultValue) {
-			return $window.localStorage[key] || defaultValue;
-		},
-		setObject : function(key, value) {
-			$window.localStorage[key] = JSON.stringify(value);
-		},
-		getObject : function(key) {
-			if ($window.localStorage[key] === "undefined") {
-				return {};
-			} else {
-				return JSON.parse($window.localStorage[key] || '{}');
-			}
-		}
-	}
-} ]);
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+});
