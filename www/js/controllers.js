aa5285ed1c6d64df0ec5a8ad41ed4ac6821ac2b4
Malik,Salil <salilmalik92@gmail.com>
2016-04-12 00:52:42 +0530

    Login Added.

---
+++ www/js/controllers.js
@@ -0,0 +1,104 @@
+angular
+    .module('starter.controllers', [])
+
+    .controller('ChatsCtrl', function($scope, Chats) {
+      // With the new view caching in Ionic, Controllers are only called
+      // when they are recreated or on app start, instead of every page
+      // change.
+      // To listen for when this page is active (for example, to refresh
+      // data),
+      // listen for the $ionicView.enter event:
+      //
+      // $scope.$on('$ionicView.enter', function(e) {
+      // });
+
+      $scope.chats = Chats.all();
+      $scope.remove = function(chat) {
+        Chats.remove(chat);
+      };
+    })
+
+    .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+      $scope.chat = Chats.get($stateParams.chatId);
+    })
+
+    .controller('AccountCtrl', function($scope) {
+      $scope.settings = {
+        enableFriends : true
+      };
+    })
+    .controller(
+        'ProfileCtrl',
+        function($scope, $window, $state, $cordovaOauth,
+            ProfileService, UserService, $localstorage) {
+          $scope.isAuthenticated = false;
+          var user = {};
+          var user = $localstorage.getObject('user');
+          if (user.name === undefined) {
+
+          } else {
+            $scope.isAuthenticated = true;
+            $scope.email = user.email;
+            $scope.name = user.name;
+            $scope.imageURL = user.imageURL;
+          }
+          $scope.googleLogin = function() {
+            $cordovaOauth
+                .google(
+                    "673944921770-cv6rdg7rgtcqu4p15ob12brc3v1u6963.apps.googleusercontent.com",
+                    [ "email" ])
+                .then(
+                    function(result) {
+                      $scope.access_token = result.access_token;
+                      ProfileService
+                          .getProfileGoogle(
+                              $scope.access_token)
+                          .success(
+                              function(data) {
+                                user.name = data.displayName;
+                                user.email = data.emails[0].value;
+                                user.imageURL = data.image.url
+                                saveUserData(user);
+                              });
+                    }, function(error) {
+                    });
+          }
+          function saveUserData(user) {
+            UserService.saveUserData(user).success(function(data) {
+              $scope.userID = data.objectId;
+              $localstorage.setObject('user', user);
+              $state.go($state.current, {}, {
+                reload : true
+              });
+            });
+          }
+          $scope.facebookLogin = function() {
+            $cordovaOauth
+                .facebook(
+                    "758110324290388",
+                    [ "email", "public_profile",
+                        "user_friends" ])
+                .then(
+                    function(result) {
+                      $scope.access_token = result.access_token;
+                      ProfileService
+                          .getProfileFacebook(
+                              $scope.access_token)
+                          .success(
+                              function(data) {
+                                user.name = data.name;
+                                user.email = data.email;
+                                user.imageURL = data.picture.data.url;
+                                saveUserData(user);
+                              });
+                    }, function(error) {
+                    });
+          }
+          $scope.logout = function() {
+            $localstorage.setObject('user', undefined);
+            $scope.isAuthenticated = false;
+            $state.go($state.current, {}, {
+              reload : true
+            });
+          }
+        });
---
+++ www/js/services.js
@@ -0,0 +1,112 @@
+angular
+    .module('starter.services', [])
+    .factory(
+        'ProfileService',
+        [
+            '$http',
+            function($http) {
+              return {
+                getProfileGoogle : getProfileGoogle,
+                getProfileFacebook : getProfileFacebook
+              };
+
+              function getProfileGoogle(access_token) {
+                return $http({
+                  method : 'GET',
+                  url : 'https://www.googleapis.com/plus/v1/people/me?access_token='
+                      + access_token
+                })
+              }
+              ;
+
+              function getProfileFacebook(access_token) {
+                console.log('access_token' + access_token);
+                return $http({
+                  method : 'GET',
+                  url : 'https://graph.facebook.com/me?fields=name,email,gender,age_range,picture,location&access_token='
+                      + access_token
+                })
+              }
+              ;
+
+            } ]).factory('UserService', [ '$http', function($http) {
+      return {
+        saveUserData : saveUserData
+      };
+
+      function saveUserData(user) {
+        return $http({
+          method : 'POST',
+          url : 'http://54.69.21.130/api/socialLogin',
+          data : user
+        })
+      }
+      ;
+
+    } ]).factory('$localstorage', [ '$window', function($window) {
+      return {
+        set : function(key, value) {
+          $window.localStorage[key] = value;
+        },
+        get : function(key, defaultValue) {
+          return $window.localStorage[key] || defaultValue;
+        },
+        setObject : function(key, value) {
+          $window.localStorage[key] = JSON.stringify(value);
+        },
+        getObject : function(key) {
+          if ($window.localStorage[key] === "undefined") {
+            return {};
+          } else {
+            return JSON.parse($window.localStorage[key] || '{}');
+          }
+        }
+      }
+    } ]).factory('Chats', function() {
+      // Might use a resource here that returns a JSON array
+
+      // Some fake testing data
+      var chats = [ {
+        id : 0,
+        name : 'Ben Sparrow',
+        lastText : 'You on your way?',
+        face : 'img/ben.png'
+      }, {
+        id : 1,
+        name : 'Max Lynx',
+        lastText : 'Hey, it\'s me',
+        face : 'img/max.png'
+      }, {
+        id : 2,
+        name : 'Adam Bradleyson',
+        lastText : 'I should buy a boat',
+        face : 'img/adam.jpg'
+      }, {
+        id : 3,
+        name : 'Perry Governor',
+        lastText : 'Look at my mukluks!',
+        face : 'img/perry.png'
+      }, {
+        id : 4,
+        name : 'Mike Harrington',
+        lastText : 'This is wicked good ice cream.',
+        face : 'img/mike.png'
+      } ];
+
+      return {
+        all : function() {
+          return chats;
+        },
+        remove : function(chat) {
+          chats.splice(chats.indexOf(chat), 1);
+        },
+        get : function(chatId) {
+          for (var i = 0; i < chats.length; i++) {
+            if (chats[i].id === parseInt(chatId)) {
+              return chats[i];
+            }
+          }
+          return null;
+        }
+      };
+    });
